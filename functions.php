02aa7f4c24867f2860c1023bbe44e23f3e426a64
Jean-Guilhem Rouel <jean-gui@w3.org>
2023-11-29 12:41:00 +0100

    fix deprecation

--- functions.php
+++ functions.php
@@ -94,7 +94,7 @@ function twentyten_setup() {
 	) );
 
 	// This theme allows users to set a custom background
-	add_custom_background();
+    add_theme_support('custom-background');
 
 	// Your changeable header business starts here
 	define( 'HEADER_TEXTCOLOR', '' );
